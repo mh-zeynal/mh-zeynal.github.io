1874a01d2b5bba0e79476c95508c5f0334580898
mh-zeynal <mh.zeynal@gmail.com>
2024-07-10 09:05:50 +0330

    feat: Config interceptor on axios config

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -1,8 +1,15 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
-const axiosConfig = axios.create({
-    baseURL: 'http://api.weatherapi.com/v1',
-    params: {
-        key: '5d13abf92c884a7c9dd164731230812'
-    }
-})
+const axiosConfig = axios.create( {
+  baseURL: 'http://api.weatherapi.com/v1',
+  params: { key: '5d13abf92c884a7c9dd164731230812' }
+} );
+
+axios.interceptors.response.use(
+  ( response: AxiosResponse ) => response,
+  ( error: AxiosError ) => {
+    console.log( `data layer error code ${ error.status }: ${ error.message }` );
+  }
+);
+
+export default axiosConfig;
